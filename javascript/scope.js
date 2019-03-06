7058ce21081ff3ab8113d8cd1d606462bba3a1da
seoyeo <psy@humanwares.com>
2019-03-06 14:58:13 +0900

    정적 유효범위

--- javascript/scope.js
+++ javascript/scope.js
@@ -1,21 +1,12 @@
-var vscope = 'global'; // 전역변수
-function fscope(){
-    var vscope = 'local'; //지역변수
-    alert(vscope);
-}
-fscope();
+var i = 5;
 
-//무한반복
-function a (){
-    i = 0;
-}
-for(var i = 0; i < 5; i++){ // var i가 여기서 전역변수가 된것
-    a();
-    document.write(i);
+function a(){
+    var i = 10;
+    b();
 }
 
-
-for(var i = 0; i < 1; i++){
-    var name = 'coding everybody';
+function b(){
+    document.write(i); // 선언될때의 i 이므로 5
 }
-alert(name); // coding everybody // java와 다른 거 유의
+
+a();
