d387966a35c59d6a9d42fef1d873b5d8e1821820
seoyeo <psy@humanwares.com>
2019-03-06 15:28:34 +0900

    값으로서의 함수 - 배열, 함수 etc

--- javascript/scope.js
+++ javascript/scope.js
@@ -10,3 +10,39 @@ function b(){
 }
 
 a();
+
+// 함수가 값처럼 사용도 가능
+function cal(func, num){
+    return func(num)
+}
+function increase(num){
+    return num+1
+}
+function decrease(num){
+    return num-1
+}
+alert(cal(increase, 1));
+alert(cal(decrease, 1));
+
+
+//함수 리턴
+function cal(mode){
+    var funcs = {
+        'plus' : function(left, right){return left + right},
+        'minus' : function(left, right){return left - right}
+    }
+    return funcs[mode];
+}
+alert(cal('plus')(2,1));
+alert(cal('minus')(2,1));
+//함수 배열
+var process = [
+    function(input){ return input + 10;},
+    function(input){ return input * input;},
+    function(input){ return input / 2;}
+];
+var input = 1;
+for(var i = 0; i < process.length; i++){
+    input = process[i](input);
+}
+alert(input);
